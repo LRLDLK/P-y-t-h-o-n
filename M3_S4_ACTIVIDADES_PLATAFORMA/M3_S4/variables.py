d64e0b861d5cb21383ef7ea06cd97110fe67af4d
LRLDLK <kldelarival@gmail.com>
2023-04-21 00:30:54 -0400

    agregando actividades de comprobacion y evaluacion M3_S4

---
+++ M3_S4_ACTIVIDADES_PLATAFORMA/M3_S4/variables.py
@@ -0,0 +1,12 @@
+#declaracion de variables
+#identificados = valor
+
+#variable con valor entero
+numero = 10
+
+numero = True
+decimal =1.4
+condicional = True
+texto = "cadena texto"
+
+print(numero)
